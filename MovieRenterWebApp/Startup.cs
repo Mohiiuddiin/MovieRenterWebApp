e8226e7d17ce12c9544cfb3407cb05299194cbaf
Mohiiuddiin <45856100+Mohiiuddiin@users.noreply.github.com>
2021-03-24 11:58:48 +0600

    Add project files.

---
+++ MovieRenterWebApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieRenterWebApp.Startup))]
+namespace MovieRenterWebApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
